f9e62190f13ea3c595178864e22bf63bcebebff5
mirzapurnandi <mirza.purnandi@gmail.com>
2022-08-25 14:56:05 +0700

    CORE

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+    <style>
+        ol>li {
+            font-size: 20px;
+            margin: 20px;
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>:: LIST TEMPLATE RESPONSIVE ::</h1>
+    <h1>::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::</h1>
+    <ol>
+        <li><a href="/template1">Template 1 (College and School Education Website)</a></li>
+        <li><a href="/template2">Template 2(Edubin Education html template)</a></li>
+        <li><a href="/template3">Template 3(Education Website and AdminPanel)</a></li>
+        <li><a href="/template4">Template 4(EduChamp Education HTML Template Admin Dashboard)</a></li>
+        <li><a href="/template5">Template 5(Eduwise Education Bootstrap4)</a></li>
+        <li><a href="/template6">Template 6(Learning Management University Website)</a></li>
+        <li><a href="/template7">Template 7(LearnMate Learning Courses Education)</a></li>
+        <li><a href="/template8">Template 8(LIBRARIA Online Library)</a></li>
+        <li><a href="/template9">Template 9(Education City Education HTML Template)</a></li>
+    </ol>
+    <h1>::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::</h1>
+</body>
+
+</html>
