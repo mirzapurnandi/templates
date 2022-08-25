d59e00098dd5221aa9803f848d53c17480539e56
mirzapurnandi <mirza.purnandi@gmail.com>
2022-08-25 15:25:38 +0700

    edit minor

--- index.php
+++ index.php
@@ -20,14 +20,14 @@
     <h1>::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::</h1>
     <ol>
         <li><a href="/template1">Template 1 (College and School Education Website)</a></li>
-        <li><a href="/template2">Template 2(Edubin Education html template)</a></li>
-        <li><a href="/template3">Template 3(Education Website and AdminPanel)</a></li>
-        <li><a href="/template4">Template 4(EduChamp Education HTML Template Admin Dashboard)</a></li>
-        <li><a href="/template5">Template 5(Eduwise Education Bootstrap4)</a></li>
-        <li><a href="/template6">Template 6(Learning Management University Website)</a></li>
-        <li><a href="/template7">Template 7(LearnMate Learning Courses Education)</a></li>
-        <li><a href="/template8">Template 8(LIBRARIA Online Library)</a></li>
-        <li><a href="/template9">Template 9(Education City Education HTML Template)</a></li>
+        <li><a href="/template2">Template 2 (Edubin Education html template)</a></li>
+        <li><a href="/template3">Template 3 (Education Website and AdminPanel)</a></li>
+        <li><a href="/template4">Template 4 (EduChamp Education HTML Template Admin Dashboard)</a></li>
+        <li><a href="/template5">Template 5 (Eduwise Education Bootstrap4)</a></li>
+        <li><a href="/template6">Template 6 (Learning Management University Website)</a></li>
+        <li><a href="/template7">Template 7 (LearnMate Learning Courses Education)</a></li>
+        <li><a href="/template8">Template 8 (LIBRARIA Online Library)</a></li>
+        <li><a href="/template9">Template 9 (Education City Education HTML Template)</a></li>
     </ol>
     <h1>::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::</h1>
 </body>
